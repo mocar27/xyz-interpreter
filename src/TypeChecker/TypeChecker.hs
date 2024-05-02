6751b2a4cb08405c32f83c54ef2e3aaf9adba630
mocar27 <komocarski@gmail.com>
2024-05-02 19:10:36 +0200

    init typechecker

---
+++ src/TypeChecker/TypeChecker.hs
@@ -0,0 +1,10 @@
+module TypeChecker.TypeChecker 
+    where
+
+import qualified Data.Map                  as Map
+import qualified Data.Set                  as Set
+import qualified Data.List                 as List
+import qualified Data.Maybe                as Maybe
+import qualified Data.Either               as Either
+import qualified Control.Monad             as Monad
+import qualified ParserLexer.AbsXyzGrammar as AbsXyzGrammar
