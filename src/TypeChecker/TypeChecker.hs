373becf5295d1fd3234de77986658e4615a3b473
mocar27 <komocarski@gmail.com>
2024-05-09 02:57:33 +0200

    working decl type

--- src/TypeChecker/TypeChecker.hs
+++ src/TypeChecker/TypeChecker.hs
@@ -1,157 +1,203 @@
 module TypeChecker.TypeChecker where
 
-import ParserLexer.AbsXyzGrammar as AbsXyzGrammar
+import ParserLexer.AbsXyzGrammar
 import Data.Map                  as Map
+import Data.Functor.Identity     (Identity)
 -- -- import qualified Data.List                 as List
 -- -- import qualified Data.Maybe                as Maybe
 -- -- import qualified Data.Either               as Either
-import qualified Control.Monad   as Monad
+import Control.Monad
 import Control.Monad.State
 import Control.Monad.Except
 import Control.Monad.Reader
+import Data.Typeable             (Typeable, typeOf)
+import Control.Exception (throw)
+
+-- | Types
+type TType = Type' ()
 
 -- | Environment mapping variable names to types.
-type Env = Map.Map String Type
+type Env = Map.Map String TType
+initialEnv :: Env
+initialEnv = Map.empty
+
+-- Function to add a variable to the global environment.
+addVariable :: String -> TType -> TypeChecker ()
+addVariable var t = do
+  env <- ask
+  let updatedEnv = Map.insert var t env
+  local (const updatedEnv) (return ())
 
 -- | The type of the type checker.
+-- -- | The type of the type checker.
+-- type TypeChecker a = ReaderT Env (ExceptT String Identity) a
+
+-- -- | Run the type checker.
+-- runTypeChecker :: Program -> Either String ()
+-- runTypeChecker program = runIdentity . (runExceptT . (`evalStateT` initialEnv)) . typeCheckProgram
 type TypeChecker a = ReaderT Env (ExceptT String (State Int)) a
 
 -- | Run the type checker.
-runTypeChecker :: AbsXyzGrammar.Program -> Either String ()
-runTypeChecker program = evalState (runExceptT (runReaderT (typeCheckProgram program) Map.empty)) 0
+runTypeChecker :: Program -> Either String ()
+runTypeChecker program = evalState (runExceptT (runReaderT (typeCheckProgram program) initialEnv)) 0
 
 -- | Type check a program.
-typeCheckProgram :: AbsXyzGrammar.Program -> TypeChecker ()
-typeCheckProgram (AbsXyzGrammar.MyProgram _ stmts) = typeCheckStmts stmts
+typeCheckProgram :: Program -> TypeChecker ()
+typeCheckProgram (MyProgram _ stmts) = typeCheckStmts stmts
+
+-- | Type check a block.
+typeCheckBlock :: Block -> TypeChecker ()
+typeCheckBlock (StmtBlock _ stmts) = typeCheckStmts stmts
 
 -- | Type check a list of statements.
-typeCheckStmts :: [AbsXyzGrammar.Stmt] -> TypeChecker ()
+typeCheckStmts :: [Stmt] -> TypeChecker ()
 typeCheckStmts [] = return ()
 typeCheckStmts (stmt : stmts) = do
   typeCheckStmt stmt
   typeCheckStmts stmts
 
 -- | Type check a statement.
-typeCheckStmt :: AbsXyzGrammar.Stmt -> TypeChecker ()
-typeCheckStmt (AbsXyzGrammar.Empty _) = return ()
-typeCheckStmt (AbsXyzGrammar.Decl _ t items) = do
-  typeCheckType t
-  typeCheckItems items
-typeCheckStmt (AbsXyzGrammar.Assign _ _ _) = return ()
-typeCheckStmt (AbsXyzGrammar.Ret _ e) = do
+typeCheckStmt :: Stmt -> TypeChecker ()
+typeCheckStmt (Empty _) = return ()
+typeCheckStmt (Decl _ t items) = do
+  let declType = omitPosition t
+  typeCheckItems declType items
+
+typeCheckStmt (Assign i v e) = do
+  throwError $ "Error: " ++ show (typeOf e)
+  -- expectedType <- getVarFromIdent v
+  -- throwError $ "Error: " ++ show expectedType
+  -- actualType <- typeCheckExpr e
+  -- unless (actualType == expectedType) $ throwError $ "Type mismatch: " ++ show actualType ++ " cannot be assigned to " ++ show expectedType
+typeCheckStmt (Ret _ e) = do
   typeCheckExpr e
   return ()
-typeCheckStmt (AbsXyzGrammar.VoidRet _) = return ()
-typeCheckStmt (AbsXyzGrammar.If _ e block) = do
+typeCheckStmt (VoidRet _) = return ()
+typeCheckStmt (If _ e block) = do
   typeCheckExpr e
   typeCheckBlock block
-typeCheckStmt (AbsXyzGrammar.IfElse _ e block1 block2) = do
+typeCheckStmt (IfElse _ e block1 block2) = do
     typeCheckExpr e
     typeCheckBlock block1
     typeCheckBlock block2
-typeCheckStmt (AbsXyzGrammar.While _ e block) = do
+typeCheckStmt (While _ e block) = do
     typeCheckExpr e
     typeCheckBlock block
-typeCheckStmt (AbsXyzGrammar.FunctionDef _ t i args block) = do
-    typeCheckType t
+typeCheckStmt (FunctionDef _ t i args block) = do
+    -- let declType = omitPosition t
     typeCheckArgs args
     typeCheckBlock block
-typeCheckStmt (AbsXyzGrammar.StmtExp _ e) = do
+typeCheckStmt (StmtExp _ e) = do
     typeCheckExpr e
     return ()
 
 -- | Type check a list of items.
-typeCheckItems :: [AbsXyzGrammar.Item] -> TypeChecker ()
-typeCheckItems [] = return ()
-typeCheckItems (item : items) = do
-  typeCheckItem item
-  typeCheckItems items
+typeCheckItems :: TType -> [Item] -> TypeChecker ()
+typeCheckItems eT [] = return ()
+typeCheckItems eT (item : items) = do
+  typeCheckItem eT item
+  typeCheckItems eT items
 
 -- | Type check an item.
-typeCheckItem :: AbsXyzGrammar.Item -> TypeChecker ()
-typeCheckItem (AbsXyzGrammar.NoInit _ _) = return ()
-typeCheckItem (AbsXyzGrammar.Init _ _ e) = do
-  typeCheckExpr e
-  return ()
-
--- | Type check a block.
-typeCheckBlock :: AbsXyzGrammar.Block -> TypeChecker ()
-typeCheckBlock (AbsXyzGrammar.StmtBlock _ stmts) = typeCheckStmts stmts
+typeCheckItem :: TType -> Item -> TypeChecker ()
+typeCheckItem eT (NoInit _ _) = return ()
+typeCheckItem eT (Init p var e) = do
+  let variableName = getVarFromIdent var
+  actualType <- typeCheckExpr e
+  unless (actualType == eT) $ throwError $ "Type mismatch: " ++ show actualType ++ " cannot be assigned to " ++ show eT
 
 -- | Type check a list of arguments.
-typeCheckArgs :: [AbsXyzGrammar.Arg] -> TypeChecker ()
+typeCheckArgs :: [Arg] -> TypeChecker ()
 typeCheckArgs [] = return ()
 typeCheckArgs (arg : args) = do
   typeCheckArg arg
   typeCheckArgs args
 
 -- | Type check an argument.
-typeCheckArg :: AbsXyzGrammar.Arg -> TypeChecker ()
-typeCheckArg (AbsXyzGrammar.ArgVal _ t _) = typeCheckType t
-typeCheckArg (AbsXyzGrammar.ArgRef _ t _) = typeCheckType t
-
--- | Type check a type.
-typeCheckType :: AbsXyzGrammar.Type -> TypeChecker ()
-typeCheckType (AbsXyzGrammar.Integer _) = return ()
-typeCheckType (AbsXyzGrammar.String _) = return ()
-typeCheckType (AbsXyzGrammar.Boolean _) = return ()
-typeCheckType (AbsXyzGrammar.Void _) = return ()
-typeCheckType (AbsXyzGrammar.Function _ t ts) = do
-  typeCheckType t
-  typeCheckTypes ts
+typeCheckArg :: Arg -> TypeChecker ()
+typeCheckArg (ArgVal _ t _) = typeCheckType t
+typeCheckArg (ArgRef _ t _) = typeCheckType t
 
 -- | Type check a list of types.
-typeCheckTypes :: [AbsXyzGrammar.Type] -> TypeChecker ()
-typeCheckTypes [] = return ()
-typeCheckTypes (t : ts) = do
-  typeCheckType t
-  typeCheckTypes ts
+-- typeCheckTypes :: [Type] -> TypeChecker ()
+-- typeCheckTypes [] = return ()
+-- typeCheckTypes (t : ts) = do
+--   typeCheckType t
+--   typeCheckTypes ts
 
--- | Type check an expression.
-typeCheckExpr :: AbsXyzGrammar.Expr -> TypeChecker ()
-typeCheckExpr (AbsXyzGrammar.ExpVar _ _) = return ()
-typeCheckExpr (AbsXyzGrammar.ExpLitInt _ _) = return ()
-typeCheckExpr (AbsXyzGrammar.ExpString _ _) = return ()
-typeCheckExpr (AbsXyzGrammar.ExpLitTrue _) = return ()
-typeCheckExpr (AbsXyzGrammar.ExpLitFalse _) = return ()
-typeCheckExpr (AbsXyzGrammar.ExpApp _ _ es) = do
-  typeCheckExprs es
-  return ()
-typeCheckExpr (AbsXyzGrammar.ExpNeg _ e) = do
-    typeCheckExpr e
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpNot _ e) = do
-    typeCheckExpr e
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpMul _ e1 _ e2) = do
-    typeCheckExpr e1
-    typeCheckExpr e2
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpAdd _ e1 _ e2) = do
-    typeCheckExpr e1
-    typeCheckExpr e2
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpRel _ e1 _ e2) = do
-    typeCheckExpr e1
-    typeCheckExpr e2
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpAnd _ e1 e2) = do
-    typeCheckExpr e1
-    typeCheckExpr e2
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpOr _ e1 e2) = do
-    typeCheckExpr e1
-    typeCheckExpr e2
-    return ()
-typeCheckExpr (AbsXyzGrammar.ExpLambda _ args t b) = do
-    typeCheckArgs args
-    typeCheckType t
-    typeCheckBlock b
-    return ()
+-- | Type check a type.
+typeCheckType :: Type -> TypeChecker ()
+typeCheckType (Integer _) = return ()
+typeCheckType (String _) = return ()
+typeCheckType (Boolean _) = return ()
+typeCheckType (Void _) = return ()
+typeCheckType (Function _ retType argTypes) = do
+  typeCheckType retType
+  mapM_ typeCheckType argTypes
 
 -- | Type check a list of expressions.
-typeCheckExprs :: [AbsXyzGrammar.Expr] -> TypeChecker ()
+typeCheckExprs :: [Expr] -> TypeChecker ()
 typeCheckExprs [] = return ()
 typeCheckExprs (e : es) = do
   typeCheckExpr e
   typeCheckExprs es
+
+-- | Type check an expression.
+typeCheckExpr :: Expr -> TypeChecker TType
+typeCheckExpr (ExpVar _ var) = getTypeFromEnv var
+typeCheckExpr (ExpLitInt _ _) = return $ Integer ()
+typeCheckExpr (ExpString _ _) = return $ String ()
+typeCheckExpr (ExpLitTrue _) = return $ Boolean ()
+typeCheckExpr (ExpLitFalse _) = return $ Boolean ()
+typeCheckExpr (ExpApp _ _ es) = do
+  typeCheckExprs es
+  return $ Integer ()
+typeCheckExpr (ExpNeg _ e) = do
+    typeCheckExpr e
+    return $ Integer ()
+typeCheckExpr (ExpNot _ e) = do
+    typeCheckExpr e
+    return $ Integer ()
+typeCheckExpr (ExpMul _ e1 _ e2) = do
+    typeCheckExpr e1
+    typeCheckExpr e2
+    return $ Integer ()
+typeCheckExpr (ExpAdd _ e1 _ e2) = do
+    typeCheckExpr e1
+    typeCheckExpr e2
+    return $ Integer ()
+typeCheckExpr (ExpRel _ e1 _ e2) = do
+    typeCheckExpr e1
+    typeCheckExpr e2
+    return $ Integer ()
+typeCheckExpr (ExpAnd _ e1 e2) = do
+    typeCheckExpr e1
+    typeCheckExpr e2
+    return $ Integer ()
+typeCheckExpr (ExpOr _ e1 e2) = do
+    typeCheckExpr e1
+    typeCheckExpr e2
+    return $ Integer ()
+typeCheckExpr (ExpLambda _ args t b) = do
+    typeCheckArgs args
+    -- omitPosition t
+    typeCheckBlock b
+    return $ Integer ()
+
+-- | Get the type of a variable from the environment.
+getTypeFromEnv :: Ident -> TypeChecker TType
+getTypeFromEnv (Ident var) = do
+  maybeType <- asks (Map.lookup var)
+  case maybeType of
+    Just t  -> return t
+    Nothing -> throwError $ "Variable " ++ var ++ " not declared"
+
+getVarFromIdent :: Ident -> String
+getVarFromIdent (Ident var) = var
+
+omitPosition :: Type' a -> TType
+omitPosition (Integer _) = Integer ()
+omitPosition (String _) = String ()
+omitPosition (Boolean _) = Boolean ()
+omitPosition (Void _) = Void ()
+omitPosition (Function _ retType argTypes) = Function () (omitPosition retType) (fmap omitPosition argTypes)
